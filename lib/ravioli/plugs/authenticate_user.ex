24eedb634a5487ceb643b544b9349c412b85e612
Michał Chatłas <michal.chatlas@espeo.eu>
2016-11-22 11:08:32 +0100

    And I think to myself, what a wonderfull world

--- lib/ravioli/plugs/authenticate_user.ex
+++ lib/ravioli/plugs/authenticate_user.ex
@@ -7,10 +7,8 @@ defmodule Ravioli.Plugs.AuthenticateUser do
   def call(%Plug.Conn{} = conn, _options) do
   	with token when is_binary(token) <- Enum.join(get_req_header(conn, "x-auth-token")),
     %User{} = user <- Repo.get_by(User, auth_token: token)
-  	  do
-  	    conn |> assign(:current_user, user)   
-   	  else 
-  	    e -> conn |> put_status(:unauthorised) |> halt
-  	end  
+  	do conn |> assign(:current_user, user)   
+   	else _ -> conn |> send_resp(:unauthorized, "") |> halt
+   	end 
   end 
 end
