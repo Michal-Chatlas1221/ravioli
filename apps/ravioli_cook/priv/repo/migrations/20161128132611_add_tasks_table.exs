8ee464438fd33111727101d65b7b30da75e883ea
Michał Chatłas <michal.chatlas@espeo.eu>
2016-11-28 15:29:24 +0100

    Switched from integer as id to uuid

--- apps/ravioli_cook/priv/repo/migrations/20161128132611_add_tasks_table.exs
+++ apps/ravioli_cook/priv/repo/migrations/20161128132611_add_tasks_table.exs
@@ -2,10 +2,11 @@ defmodule RavioliCook.Repo.Migrations.AddTasksTable do
   use Ecto.Migration
 
   def change do
-  	create table(:tasks) do
+  	create table(:tasks, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :input, :json
       add :result, :json
-      add :batch_id, references(:batches, on_delete: :delete_all)
+      add :batch_id, references(:batches, on_delete: :delete_all, type: :uuid)
 
       timestamps()
     end
